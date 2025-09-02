54fe12f1bb352b3b44806b3dc1426e236734fa2e
Your Name <you@example.com>
2025-09-03 01:24:07 +0600

    checking image upload1

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,29 +1,43 @@
-import { useEffect, useState } from "react";
-import Project from "./Project";
-import axios from "axios";
+import { motion } from "framer-motion";
+import { Link } from "react-router";
 
-const Projects = () => {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    axios
-      .get("projects.json")
-      .then((res) => {
-        setItems(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+const Project = ({ item }) => {
+  const { id, title, head, description } = item;
+
+  // Head an array
+  const images = Array.isArray(head) ? head : [head];
+
   return (
-    <div className="border-b border-neutral-500 pb-4">
-      <h1 className="text-4xl text-center my-20">Projects</h1>
-      <div>
-        {items.map((item) => (
-          <Project key={item.id} item={item}></Project>
-        ))}
+    <Link to={`/projects/${id}`} state={{ item }}>
+      <div className="mb-8 flex flex-wrap lg:justify-center">
+        {/* Horizontal image row */}
+        <div className="w-full lg:w-1/3 flex gap-2 overflow-x-auto">
+          {images.map((img, index) => (
+            <motion.img
+              key={index}
+              initial={{ x: -50, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+              src={img}
+              alt={`${title} preview ${index + 1}`}
+              className="rounded object-contain w-[140px] h-[180px] flex-shrink-0"
+            />
+          ))}
+        </div>
+
+        {/* Text info */}
+        <motion.div
+          className="w-full max-w-xl lg:w-1/4 mt-4 lg:mt-0 lg:ml-6"
+          initial={{ x: 100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h6 className="mb-6 font-semibold text-xl">{title}</h6>
+          <p className="mb-4 text-neutral-400">{description}</p>
+        </motion.div>
       </div>
-    </div>
+    </Link>
   );
 };
 
-export default Projects;
+export default Project;
