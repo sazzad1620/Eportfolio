89be1f4e11e54bac4a35235540f29644ee9b21c0
Your Name <you@example.com>
2025-09-02 16:58:21 +0600

    project details page update1

--- src/components/projects/ProjectDetails.jsx
+++ src/components/projects/ProjectDetails.jsx
@@ -8,6 +8,13 @@ const ProjectDetails = () => {
 
   return (
     <div className="container mx-auto max-w-7xl mt-8">
+      
+      <div className="text">
+        <h1 className="font-bold text-2xl">{item.title}</h1>
+        <p className="py-6 font-semibold text-xl">{item.description}</p>
+        {item.details && <p>{item.details}</p>}
+        
+      </div>
       <div className="images grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-5">
         {item.imgs?.length > 0 ? (
           item.imgs.map((img, index) => (
@@ -15,12 +22,8 @@ const ProjectDetails = () => {
           ))
         ) : (
           <img src={item.head} alt={item.title} />
-        )}
-      </div>
-      <div className="text">
-        <h1 className="font-bold text-2xl">{item.title}</h1>
-        <p className="py-6 font-semibold text-xl">{item.description}</p>
-        {item.details && <p>{item.details}</p>}
+        )
+        }
         <Link to="/projects">
           <button className="btn mt-5">Back</button>
         </Link>
